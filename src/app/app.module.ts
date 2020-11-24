cfca5037a4c107ebe06480131cd673e9a386aa74
obidano <omotetedan2@gmail.com>
2020-11-24 15:40:35 +0100

    contact view #4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import {NgModule} from '@angular/core';
 
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from "./app-routing.module";
-import { ContactsComponent } from './pages/contacts/contacts.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {ContactsComponent} from './pages/contacts/contacts.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatInputModule} from "@angular/material/input";
 import {MatButtonModule} from "@angular/material/button";
 import {MatIconModule} from "@angular/material/icon";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatTableModule} from "@angular/material/table";
+import {MatMenuModule} from "@angular/material/menu";
+import {MatRippleModule} from "@angular/material/core";
 
 @NgModule({
   declarations: [
@@ -22,7 +26,11 @@ import {MatIconModule} from "@angular/material/icon";
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
-    MatIconModule
+    MatIconModule,
+    MatTableModule,
+    MatMenuModule,
+    ReactiveFormsModule,
+    MatRippleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/contacts/contacts.component.ts
+++ src/app/pages/contacts/contacts.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {FormBuilder, FormGroup} from "@angular/forms";
+import {MatTableDataSource} from "@angular/material/table";
 
 @Component({
   selector: 'app-contacts',
   templateUrl: './contacts.component.html',
-  styleUrls: ['./contacts.component.css']
+  styleUrls: ['./contacts.component.css'],
 })
 export class ContactsComponent implements OnInit {
+  searchForm: FormGroup;
 
-  constructor() { }
+  dataSource: MatTableDataSource<any> | null;
+  displayedColumns = ['id', 'nom', 'prenom', 'postnom', 'phone', 'societe', 'email', 'anniversaire', 'actions'];
+
+  constructor(private _fb: FormBuilder,) {
+  }
 
   ngOnInit(): void {
+    this.searchForm = this._fb.group({
+      search: [],
+    });
   }
 
 }
